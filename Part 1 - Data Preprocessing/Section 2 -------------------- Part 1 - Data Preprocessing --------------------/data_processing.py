7784664cb7099fa8a27f1c3bf7418e43b50cd6df
Gavin <gavinmwong@gmail.com>
2019-12-18 17:50:44 -0800

    finished secion 2

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing.py
@@ -1,4 +1,4 @@
-# Data Preprocessing Template
+# Data Preprocessing
 
 # Importing the libraries
 import numpy as np
@@ -26,19 +26,20 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
-## Importing the dataset
-#dataset = pd.read_csv('Data.csv')
-#X = dataset.iloc[:, :-1].values
-#y = dataset.iloc[:, 3].values
-#
-## Splitting the dataset into the Training set and Test set
-#from sklearn.cross_validation import train_test_split
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-#
-## Feature Scaling
+# Splitting the dataset into Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
 #"""from sklearn.preprocessing import StandardScaler
 #sc_X = StandardScaler()
 #X_train = sc_X.fit_transform(X_train)
 #X_test = sc_X.transform(X_test)
 #sc_y = StandardScaler()
-#y_train = sc_y.fit_transform(y_train)"""
+#y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
