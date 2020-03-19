2b579ef5be556fec6adee90f8b4eed45a24d5314
Gavin <gavinmwong@gmail.com>
2020-03-18 22:13:00 -0700

    minor changes

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -9,14 +9,15 @@ import pandas as pd
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = "\t", quoting = 3)
 
 # Cleaning the texts
+
 import re
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 corpus = []
-for i in range(0, 1000):
-    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+for i in range(1000):
+    review = re.sub("[^a-zA-z])", ' ', dataset["Review"][i])
     review = review.lower()
     review = review.split()
     ps = PorterStemmer()
