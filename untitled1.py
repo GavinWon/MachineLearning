0e95b414406da9530b74774880d217b77011b323
Gavin <gavinmwong@gmail.com>
2020-06-15 12:21:43 -0700

    small change

---
+++ untitled1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 00:27:22 2020
+
+@author: Gavin
+"""
+
+def test(a, c, b=10, d=200):
+    print(a)
+    print(c)
+    print(b)
+    print(d)
+test(5, 100, d=99, b=250)
