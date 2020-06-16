918ae76254195fb9452af3178b8080c2bd5f491c
Gavin <gavinmwong@gmail.com>
2020-06-16 11:42:49 -0700

    added metric

--- Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
+++ Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
@@ -22,15 +22,17 @@ X_test = sc.transform(X_test)
 
 # Fitting SVM to the Training set
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
+classifier = SVC(kernel='linear', random_state=0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
