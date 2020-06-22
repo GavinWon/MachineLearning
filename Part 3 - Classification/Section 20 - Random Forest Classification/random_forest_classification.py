1333e58ad4539664ddefe853b02f1bea6a1f4d0a
Gavin <gavinmwong@gmail.com>
2020-06-22 15:04:48 -0700

    random forest finish

--- Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.py
+++ Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.py
@@ -7,8 +7,9 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+X = dataset.iloc[:, [2, 3]]
+y = dataset.iloc[:, 4]
+print(X)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -29,8 +30,10 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
