ec32fa798f380fb593c1288df297bb12fd95b132
Gavin <gavinmwong@gmail.com>
2020-10-07 22:54:54 -0700

    renamed files

--- Classification/Section 20 - Random Forest Classification/random_forest_classification.py
+++ Classification/Section 20 - Random Forest Classification/random_forest_classification.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]]
-y = dataset.iloc[:, 4]
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
 print(X)
 
 # Splitting the dataset into the Training set and Test set
